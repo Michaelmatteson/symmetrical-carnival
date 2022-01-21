94145be12ddceadbff77c7863630bcd828d9ae3a
Michaelmatteson <93003020+Michaelmatteson@users.noreply.github.com>
2022-01-21 09:51:10 -0800

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,14 @@
+var score = 0;
+function updateScore()
+{
+    score = score +1;
+    document.getElementById("score").innerHTML = score;
+}
+function saveScore()
+{
+    localStorage.setItem("score",score)
+}
+function nextPage()
+{
+    window.location = "activity_2.html"
+}
